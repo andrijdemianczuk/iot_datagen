9926d0b571906fdaccf5fa8f57db731d8f7cd8ae
Andrij Demianczuk <andrij.demianczuk@databricks.com>
2022-01-25 12:29:33 -0700

    fixed minor format issues

--- main.py
+++ main.py
@@ -11,18 +11,20 @@ if __name__ == '__main__':
     #f1.write("Now the file has more content!")
     #f1.close()
 
-    # with open('IoT_Sensor_Template/Humidity.csv') as f:
-    #    reader = csv.reader(f)
-    #    epoch_time = int(time.time())
-    #    for row in reader:
-    #        if (row[2] == "0"):
-    #            row[2] = epoch_time
-    #        if (row[1] == "0"):
-    #            row[1] = random.randint(0,100)
-    #        f1.write(str(row))
-    #        f1.write("\r\n")
-    #
-    # f1.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     with open('IoT_Sensor_Template/Humidity.csv') as f:
         reader = csv.reader(f)
@@ -33,9 +35,43 @@ if __name__ == '__main__':
             if (row[1] == "0"):
                 row[1] = random.randint(0,100)
             rowStr = str(row)
-            f1.write(str(row).translate({ord(i): None for i in '[]'}))
+            f1.write(str(row).translate({ord(i): None for i in '[]\''})) #Remove the unwanted characters '[', ']' and '''
             f1.write("\r\n")
     f1.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # with open('IoT_Sensor_Template/Light.csv') as f:
     #     reader = csv.reader(f)
